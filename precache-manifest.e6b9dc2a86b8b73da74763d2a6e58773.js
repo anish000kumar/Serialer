c09b79567dee44213d904145681e94e5bc972f2e
Anish Kumar <akmarhw@amazon.com>
2019-10-03 22:28:55 +0530

    Updates

---
+++ precache-manifest.e6b9dc2a86b8b73da74763d2a6e58773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7985bc4efa0dbd476d33a1b1b8e7c4e8",
+    "url": "/Serialer/index.html"
+  },
+  {
+    "revision": "82c0d30e800504485885",
+    "url": "/Serialer/static/css/main.979a2cff.chunk.css"
+  },
+  {
+    "revision": "35f16a45c55a40f3deac",
+    "url": "/Serialer/static/js/2.bf44beef.chunk.js"
+  },
+  {
+    "revision": "82c0d30e800504485885",
+    "url": "/Serialer/static/js/main.ac569eb0.chunk.js"
+  },
+  {
+    "revision": "7ea34ea03df4e7bd42e8",
+    "url": "/Serialer/static/js/runtime-main.04336a1d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Serialer/static/media/logo.25bf045c.svg"
+  }
+]);
